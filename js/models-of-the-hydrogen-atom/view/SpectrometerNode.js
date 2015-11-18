16952e0224977b56eb1f9e2d60f441edd227d1f9
Chris Malley <cmalley@pixelzoom.com>
2015-11-18 13:56:46 -0700

    add buttons to spectrometer

--- js/models-of-the-hydrogen-atom/view/SpectrometerNode.js
+++ js/models-of-the-hydrogen-atom/view/SpectrometerNode.js
@@ -10,14 +10,32 @@ define( function( require ) {
 
   // modules
   var AccordionBox = require( 'SUN/AccordionBox' );
+  var Dimension2 = require( 'DOT/Dimension2' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
+  var Image = require( 'SCENERY/nodes/Image' );
   var inherit = require( 'PHET_CORE/inherit' );
   var MHAFont = require( 'MODELS_OF_THE_HYDROGEN_ATOM/models-of-the-hydrogen-atom/view/MHAFont' );
   var modelsOfTheHydrogenAtom = require( 'MODELS_OF_THE_HYDROGEN_ATOM/modelsOfTheHydrogenAtom' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
+  var RectangularPushButton = require( 'SUN/buttons/RectangularPushButton' );
   var Text = require( 'SCENERY/nodes/Text' );
+  var VBox = require( 'SCENERY/nodes/VBox' );
+
+  // images
+  var cameraImage = require( 'image!MODELS_OF_THE_HYDROGEN_ATOM/camera.png' );
 
   // strings
+  var resetString = require( 'string!MODELS_OF_THE_HYDROGEN_ATOM/reset' );
   var spectrometerPhotonsEmittedNmString = require( 'string!MODELS_OF_THE_HYDROGEN_ATOM/spectrometerPhotonsEmittedNm' );
+  var stopString = require( 'string!MODELS_OF_THE_HYDROGEN_ATOM/stop' );
+
+  // constants
+  var BUTTON_LABEL_OPTIONS = {
+    font: new MHAFont( 14 ),
+    maxWidth: 50 // i18n, determined empirically
+  };
+  var BUTTON_COLOR = 'rgb( 245, 245, 245 )';
+  var DISPLAY_SIZE = new Dimension2( 400, 55 );
 
   /**
    * @param {Property.<boolean>} expandedProperty
@@ -46,9 +64,48 @@ define( function( require ) {
       maxWidth: 290 // i18n, determined empirically
     } );
 
-    var contentNode = new Rectangle( 0, 0, 400, 85, {
+    var stopButton = new RectangularPushButton( {
+      baseColor: BUTTON_COLOR,
+      content: new Text( stopString, BUTTON_LABEL_OPTIONS ),
+      listener: function() {
+        //TODO
+      }
+    } );
+
+    var resetButton = new RectangularPushButton( {
+      baseColor: BUTTON_COLOR,
+      content: new Text( resetString, BUTTON_LABEL_OPTIONS ),
+      listener: function() {
+        //TODO
+      }
+    } );
+
+    var cameraButton = new RectangularPushButton( {
+      maxHeight: stopButton.height,
+      baseColor: BUTTON_COLOR,
+      content: new Image( cameraImage ),
+      listener: function() {
+        //TODO
+      }
+    } );
+
+    var buttonGroup = new HBox( {
+      spacing: 20,
+      children: [ stopButton, resetButton, cameraButton ]
+    } );
+
+    //TODO placeholder
+    var displayNode = new Rectangle( 0, 0, DISPLAY_SIZE.width, DISPLAY_SIZE.height, {
       fill: 'white',
       stroke: 'black'
+    } );
+
+    var contentNode = new VBox( {
+      spacing: 5,
+      children: [
+        displayNode,
+        buttonGroup
+      ]
     } );
 
     AccordionBox.call( this, contentNode, options );
