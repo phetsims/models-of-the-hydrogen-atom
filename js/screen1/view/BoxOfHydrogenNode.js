15ac74c57359b3f0d45c9989fce6ceb42088b0b0
Chris Malley <cmalley@pixelzoom.com>
2016-08-22 12:11:07 -0600

    update box of hydrogen to match design mockups

--- js/screen1/view/BoxOfHydrogenNode.js
+++ js/screen1/view/BoxOfHydrogenNode.js
@@ -13,17 +13,22 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var LinearGradient = require( 'SCENERY/util/LinearGradient' );
   var modelsOfTheHydrogenAtom = require( 'MODELS_OF_THE_HYDROGEN_ATOM/modelsOfTheHydrogenAtom' );
+  var MOTHAFont = require( 'MODELS_OF_THE_HYDROGEN_ATOM/common/MOTHAFont' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var Shape = require( 'KITE/Shape' );
+  var Text = require( 'SCENERY/nodes/Text' );
+
+  // strings
+  var hydrogenSymbolString = require( 'string!MODELS_OF_THE_HYDROGEN_ATOM/hydrogenSymbol' );
 
   // constants
   var BACK_DEPTH = 10;
   var BACK_OFFSET = 0.15;
-  var BOX_SIZE = new Dimension2( 70, 70 );
-  var FRONT_COLOR = 'rgb( 128, 128, 128 )';
-  var BACK_COLOR = 'rgb( 64, 64, 64 )';
+  var BOX_SIZE = new Dimension2( 70, 60 );
+  var LIGHT_COLOR = 'rgb( 249, 249, 249 )';
+  var SHADOW_COLOR = 'rgb( 100, 100, 100 )';
 
   /**
    * @param {Object} [options]
@@ -38,19 +43,28 @@ define( function( require ) {
       .lineTo( BOX_SIZE.width, BACK_DEPTH )
       .lineTo( 0, BACK_DEPTH )
       .close(), {
-      fill: new LinearGradient( 0, 0, 0, BACK_DEPTH ).addColorStop( 0, BACK_COLOR ).addColorStop( 1, FRONT_COLOR ),
+      fill: new LinearGradient( 0, 0, BOX_SIZE.width, BACK_DEPTH ).addColorStop( 0, LIGHT_COLOR ).addColorStop( 1, SHADOW_COLOR ),
       stroke: 'black',
       lineWidth: 1
     } );
 
     // front face
     var frontNode = new Rectangle( 0, BACK_DEPTH, BOX_SIZE.width, BOX_SIZE.height, {
-      fill: FRONT_COLOR,
+      fill: new LinearGradient( 0, 0, BOX_SIZE.width, 0 ).addColorStop( 0, LIGHT_COLOR ).addColorStop( 1, SHADOW_COLOR ),
       stroke: 'black',
       lineWidth: 1
     } );
 
-    options.children = [  frontNode, topNode  ];
+    // hydrogen symbol
+    var hydrogenSymbol = new Text( hydrogenSymbolString, {
+      font: new MOTHAFont( { weight: 'bold', size: 28 } ),
+      left: frontNode.left + ( 0.25 * BOX_SIZE.width ),
+      bottom: frontNode.bottom - ( 0.15 * BOX_SIZE.height ),
+      maxWidth: 0.65 * BOX_SIZE.width
+    } );
+
+    assert && assert( !options.children, 'decordation not supported' );
+    options.children = [  frontNode, topNode, hydrogenSymbol  ];
 
     Node.call( this, options );
   }
