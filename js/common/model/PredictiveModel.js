57f08e6892ba4555f69b3103aae8c549c612778a
phet-dev <phetadmin@gmail.com>
2020-01-08 03:21:17 -0700

    update copyright dates from daily grunt work

--- js/common/model/PredictiveModel.js
+++ js/common/model/PredictiveModel.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, University of Colorado Boulder
+// Copyright 2016-2020, University of Colorado Boulder
 
 /**
  * PredictiveModel is the base class for all predictive models.
