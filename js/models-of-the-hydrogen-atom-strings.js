831abe0bba92f0a8f6920acfd23465d7c7d968d8
samreid <reids@colorado.edu>
2020-02-27 12:07:56 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/models-of-the-hydrogen-atom-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import modelsOfTheHydrogenAtom from './modelsOfTheHydrogenAtom.js';
+
+const modelsOfTheHydrogenAtomStrings = getStringModule( 'MODELS_OF_THE_HYDROGEN_ATOM' );
+
+modelsOfTheHydrogenAtom.register( 'modelsOfTheHydrogenAtomStrings', modelsOfTheHydrogenAtomStrings );
+
+export default modelsOfTheHydrogenAtomStrings;
