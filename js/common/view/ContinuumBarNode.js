43c1c7f4c2efda5e1da2b54039257c12386dd998
phet-dev <phetadmin@gmail.com>
2021-07-21 03:37:07 -0600

    update copyright dates from daily grunt work

--- js/common/view/ContinuumBarNode.js
+++ js/common/view/ContinuumBarNode.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, University of Colorado Boulder
+// Copyright 2016-2021, University of Colorado Boulder
 
 /**
  * ContinuumBarNode appears next to the radio buttons for choosing predicitive models, and shows whether
--- js/common/view/ExperimentPredictionSwitch.js
+++ js/common/view/ExperimentPredictionSwitch.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2020, University of Colorado Boulder
+// Copyright 2015-2021, University of Colorado Boulder
 
 /**
  * ExperimentPredictionSwitch is an AB switch that determines whether we are viewing an experiment or a predictive model.
