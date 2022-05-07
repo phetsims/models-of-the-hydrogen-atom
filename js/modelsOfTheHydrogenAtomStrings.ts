0fd1a82558c8563faea1b74d5cc927cb7cd556bd
Sam Reid <reids@colorado.edu>
2022-05-07 09:33:09 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/modelsOfTheHydrogenAtomStrings.ts
+++ js/modelsOfTheHydrogenAtomStrings.ts
@@ -9,40 +9,40 @@ import modelsOfTheHydrogenAtom from './modelsOfTheHydrogenAtom.js';
 
 type StringsType = {
   'models-of-the-hydrogen-atom': {
-    'title': string
-  },
-  '3DView': string,
-  'billiardBall': string,
-  'bohr': string,
-  'brightnessView': string,
-  'classical': string,
-  'classicalSolarSystem': string,
-  'close': string,
-  'deBroglie': string,
-  'electron': string,
-  'electronEnergyLevel': string,
-  'experiment': string,
-  'prediction': string,
-  'fast': string,
-  'hydrogenSymbol': string,
-  'legend': string,
-  'neutron': string,
-  'normal': string,
-  'photon': string,
-  'photonsEmittedNm': string,
-  'plumPudding': string,
-  'proton': string,
-  'quantum': string,
-  'radialView': string,
-  'schrodinger': string,
+    'title': string;
+  };
+  '3DView': string;
+  'billiardBall': string;
+  'bohr': string;
+  'brightnessView': string;
+  'classical': string;
+  'classicalSolarSystem': string;
+  'close': string;
+  'deBroglie': string;
+  'electron': string;
+  'electronEnergyLevel': string;
+  'experiment': string;
+  'prediction': string;
+  'fast': string;
+  'hydrogenSymbol': string;
+  'legend': string;
+  'neutron': string;
+  'normal': string;
+  'photon': string;
+  'photonsEmittedNm': string;
+  'plumPudding': string;
+  'proton': string;
+  'quantum': string;
+  'radialView': string;
+  'schrodinger': string;
   'screen': {
-    'spectra': string,
-    'energyLevels': string
-  },
-  'showAbsorptionWavelengths': string,
-  'slow': string,
-  'spectrometer': string,
-  'viewSnapshots': string
+    'spectra': string;
+    'energyLevels': string;
+  };
+  'showAbsorptionWavelengths': string;
+  'slow': string;
+  'spectrometer': string;
+  'viewSnapshots': string;
 };
 
 const modelsOfTheHydrogenAtomStrings = getStringModule( 'MODELS_OF_THE_HYDROGEN_ATOM' ) as StringsType;
