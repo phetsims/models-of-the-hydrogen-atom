f6b0761ec8af87c2e4fbe4e5b710bad0f45bec5e
phet-dev <phetadmin@gmail.com>
2024-02-29 03:19:53 -0700

    update copyright dates from daily grunt work

--- js/common/view/DeBroglieViewComboBox.ts
+++ js/common/view/DeBroglieViewComboBox.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * DeBroglieViewComboBox is the combo box for selecting which view of the de Broglie model show be displayed.
