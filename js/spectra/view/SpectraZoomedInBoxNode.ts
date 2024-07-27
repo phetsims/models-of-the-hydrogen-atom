c3eb0dd251473bbd2daec9c41c15480694e9bbfe
phet-dev <phetadmin@gmail.com>
2024-07-27 04:08:49 -0600

    update copyright dates from daily grunt work

--- js/spectra/view/SpectraZoomedInBoxNode.ts
+++ js/spectra/view/SpectraZoomedInBoxNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * SpectraZoomedInBoxNode shows what's inside the zoomed-in box for the 'Spectra' screen.
