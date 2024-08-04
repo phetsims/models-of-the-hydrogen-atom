2b8dfe472fa0387f55b720ac1b988029e3e518e0
pixelzoom <cmalley@pixelzoom.com>
2024-08-04 19:00:14 -0400

    stroke white beam

--- js/common/view/BeamNode.ts
+++ js/common/view/BeamNode.ts
@@ -1,8 +1,7 @@
 // Copyright 2015-2024, University of Colorado Boulder
 
-//TODO white beam is not visible on white background in projector mode
 /**
- * BeamNode is the beam the comes out of the gun.
+ * BeamNode is the beam the comes out of the light.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
@@ -12,6 +11,7 @@ import optionize from '../../../../phet-core/js/optionize.js';
 import { NodeTranslationOptions, Rectangle, RectangleOptions } from '../../../../scenery/js/imports.js';
 import modelsOfTheHydrogenAtom from '../../modelsOfTheHydrogenAtom.js';
 import Light from '../model/Light.js';
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 
 type SelfOptions = {
   beamSize?: Dimension2;
@@ -31,7 +31,11 @@ export default class BeamNode extends Rectangle {
       // RectangleOptions
       isDisposable: false,
       visibleProperty: light.isOnProperty,
-      fill: light.colorProperty
+      fill: light.colorProperty,
+
+      // When displaying a white beam on a white background, a stroke is needed.
+      stroke: new DerivedProperty( [ light.lightModeProperty ], lightMode => lightMode === 'white' ? 'black' : null ),
+      lineWidth: 0.5
     }, providedOptions );
 
     super( 0, 0, options.beamSize.width, options.beamSize.height, options );
