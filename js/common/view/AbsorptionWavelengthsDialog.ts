9d5e3a1b1da1476f55dbb50f70e1d1d58e78621a
pixelzoom <cmalley@pixelzoom.com>
2024-08-04 16:23:55 -0400

    TODO

--- js/common/view/AbsorptionWavelengthsDialog.ts
+++ js/common/view/AbsorptionWavelengthsDialog.ts
@@ -78,6 +78,7 @@ export default class AbsorptionWavelengthsDialog extends Dialog {
       columnHeadings
     ];
 
+    //TODO Using a left-right arrow here implies absorption (right) AND emission (left). Should this dialog be retitled? Or use a right arrow here?
     for ( const [ wavelength, transition ] of map ) {
       rows.push( [
         new Text( `${transition.n1} \u2194 ${transition.n2}`, TEXT_OPTIONS ),
