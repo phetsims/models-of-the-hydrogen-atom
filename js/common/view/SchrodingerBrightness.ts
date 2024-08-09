5e3f25f982ecc0d9741643b655b7c12cd464577d
pixelzoom <cmalley@pixelzoom.com>
2024-08-09 17:10:59 -0400

    Revert "cleaner way of filling array with 0's"
    This reverts commit 5fd37161ad2d5a99e8c087a4d6db4420273e1e40.

--- js/common/view/SchrodingerBrightness.ts
+++ js/common/view/SchrodingerBrightness.ts
@@ -61,7 +61,11 @@ export default class SchrodingerBrightness {
     }
 
     // Initialize sums with zeros
-    this.sums = new Array( NUMBER_OF_VERTICAL_CELLS ).fill( 0 );
+    //TODO this is so ugly
+    this.sums = Array( NUMBER_OF_VERTICAL_CELLS );
+    for ( let i = 0; i < NUMBER_OF_VERTICAL_CELLS; i++ ) {
+      this.sums[ i ] = Array( NUMBER_OF_HORIZONTAL_CELLS ).fill( 0 );
+    }
 
     // 3D cell size
     this.cellWidth = ( zoomedInBoxBounds.width / NUMBER_OF_HORIZONTAL_CELLS ) / 2;
