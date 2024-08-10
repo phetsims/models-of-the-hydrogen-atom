c3af4a84bc6ab42326a1f6c2c435ead41e3204ef
phet-dev <phetadmin@gmail.com>
2024-08-10 03:23:56 -0600

    update copyright dates from daily grunt work

--- js/common/view/SchrodingerBrightness.ts
+++ js/common/view/SchrodingerBrightness.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * SchrodingerBrightness is responsible for representing Schrodinger states (and their associated wavefunctions)
