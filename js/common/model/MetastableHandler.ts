bb42a22a0e8dca887388a89207772b4c5bc52122
pixelzoom <cmalley@pixelzoom.com>
2024-08-23 16:13:24 -0600

    doc and logging

--- js/common/model/MetastableHandler.ts
+++ js/common/model/MetastableHandler.ts
@@ -7,6 +7,8 @@
  *  atom (takes the electron to a higher state, a higher value of n). While the light is emitting white light and the
  *  atom is in state (2,0,0), we fire an absorbable photon at the atom's center every MAX_STUCK_TIME milliseconds.
  *
+ *  Ported from MetastableHandler.java, with many changes.
+ *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
@@ -23,6 +25,7 @@ import PhetioObject, { PhetioObjectOptions } from '../../../../tandem/js/PhetioO
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 import Property from '../../../../axon/js/Property.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import MOTHASymbols from '../MOTHASymbols.js';
 
 const EXCITE_ATOM_INTERVAL = 0.1; // seconds
 
@@ -105,10 +108,15 @@ export default class MetastableHandler extends PhetioObject {
   // The absorption wavelength that will take the electron to a higher state (n) is chosen at random.
   public exciteAtom(): void {
     assert && assert( this.isActiveProperty.value );
+
+    // Randomly choose an absorption wavelength. See https://github.com/phetsims/models-of-the-hydrogen-atom/issues/55.
     const wavelengths = SchrodingerModel.getAbsorptionWavelengths( MetastableHandler.METASTABLE_STATE.n );
     const wavelength = wavelengths[ dotRandom.nextInt( wavelengths.length ) ];
+
+    // Tell the light to emit a photon, and direct it towards the center of the atom.
     this.light.createPhotonAtCenter( wavelength );
-    phet.log && phet.log( `MetastableHandler.exciteAtom wavelength=${wavelength}` );
+
+    phet.log && phet.log( `MetastableHandler.exciteAtom ${MOTHASymbols.lambda}=${wavelength}` );
   }
 }
 
