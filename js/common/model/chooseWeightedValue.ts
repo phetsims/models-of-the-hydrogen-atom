ce149ea789cad7682fa605a0e7c4586f559e40d3
phet-dev <phetadmin@gmail.com>
2024-08-24 03:19:01 -0600

    update copyright dates from daily grunt work

--- js/common/model/chooseWeightedValue.ts
+++ js/common/model/chooseWeightedValue.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2024, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * chooseWeightedValue randomly selects a value from a collection of weighted values.
