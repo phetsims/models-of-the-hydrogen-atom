537356dfb2aca8f9830bbf1c7c6d64b3be64bbad
phet-dev <phetadmin@gmail.com>
2024-08-27 03:18:36 -0600

    update copyright dates from daily grunt work

--- js/common/model/solveAssociatedLegendrePolynomial.ts
+++ js/common/model/solveAssociatedLegendrePolynomial.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2023, University of Colorado Boulder
+// Copyright 2022-2024, University of Colorado Boulder
 
 /**
  * Solves the associated Legendre polynomial.  This is needed to compute the wave function for the Schrodinger
