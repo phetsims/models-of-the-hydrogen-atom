1e19121dd19e8c741ac036f04ad14328654fde74
Sam Reid <reids@colorado.edu>
2024-09-25 19:32:16 -0600

    Upgrade to ESLint 9 and flat config, see https://github.com/phetsims/chipper/issues/1474

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * ESlint configuration for models-of-the-hydrogen-atom.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import parent from '../chipper/eslint/sim.eslint.config.mjs';
+
+export default [
+  ...parent,
+  {
+    rules: {
+      'phet/todo-should-have-issue': 'off'
+    }
+  },
+  {
+    files: [
+      '**/*.ts'
+    ],
+    rules: {
+      '@typescript-eslint/ban-ts-comment': [
+        'error',
+        {
+          'ts-expect-error': true,
+          'ts-ignore': true,
+          'ts-check': true,
+          'ts-nocheck': true
+        }
+      ]
+    }
+  }
+];
