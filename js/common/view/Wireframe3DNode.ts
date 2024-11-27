27c2210c862aa37b8b29dee94aac1fd6c9ef9686
pixelzoom <cmalley@pixelzoom.com>
2024-11-27 10:25:44 -0700

    TODO

--- js/common/view/Wireframe3DNode.ts
+++ js/common/view/Wireframe3DNode.ts
@@ -37,7 +37,7 @@ export default class Wireframe3DNode extends Node {
     this.wireframeModel = wireframeModel;
 
     this.path = new Path( null, {
-      stroke: this.wireframeModel.frontColorProperty, //TODO use color palette based on z coordinate
+      stroke: this.wireframeModel.frontColorProperty, //TODO use color palette (or alpha?) based on z coordinate
       lineWidth: this.wireframeModel.lineWidth
     } );
     this.addChild( this.path );
