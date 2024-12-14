3d1de19b7ad473a3d5a51135e8a167a8c82f98ca
pixelzoom <cmalley@pixelzoom.com>
2024-12-14 13:28:44 -0700

    compute electron energies

--- js/energylevels/view/ElectronEnergyLevelDiagramNode.ts
+++ js/energylevels/view/ElectronEnergyLevelDiagramNode.ts
@@ -16,9 +16,17 @@ import ModelsOfTheHydrogenAtomStrings from '../../ModelsOfTheHydrogenAtomStrings
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import optionize from '../../../../phet-core/js/optionize.js';
 import ElectronNode from '../../common/view/ElectronNode.js';
+import MOTHAConstants from '../../common/MOTHAConstants.js';
 
 const X_MARGIN = 4;
 const Y_MARGIN = 5;
+
+// Energy of the ground state (n = 1), in eV.
+const E1 = -13.6;
+
+// How much to distort the spacing between energy level values. For no distortion, set this to zero.
+// This is needed so that energy levels do not visually overlap.
+const DISTORTION_FACTOR = 0.2; // 1.0 = 100% increase in spacing
 
 type SelfOptions = {
   size: Dimension2;
@@ -29,15 +37,20 @@ type ElectronEnergyLevelDiagramNodeOptions = SelfOptions & PickRequired<NodeOpti
 export default class ElectronEnergyLevelDiagramNode extends Node {
 
   protected readonly electronNode: Node;
-  protected readonly stateLayer: Node; // layer for all state (energy-level) information
-  protected readonly squiggleLayer: Node; // layer for the squiggle that shows the path from previous to current state
+
+  // Layer for all state (energy-level) information.
+  protected readonly stateLayer: Node;
+
+  // Layer for the squiggle between the electron's previous and current state in the diagram.
+  protected readonly squiggleLayer: Node;
+
+  // Energy at each electron state in eV, indexed by n. So note that energies[0] will be undefined.
+  private readonly energies: number[];
 
   //TODO protected
   public constructor( providedOptions: ElectronEnergyLevelDiagramNodeOptions ) {
 
-    const options = optionize<ElectronEnergyLevelDiagramNodeOptions, SelfOptions, NodeOptions>()( {
-
-    }, providedOptions );
+    const options = optionize<ElectronEnergyLevelDiagramNodeOptions, SelfOptions, NodeOptions>()( {}, providedOptions );
 
     const rectangle = new Rectangle( 0, 0, options.size.width, options.size.height, {
       fill: 'white',
@@ -80,7 +93,31 @@ export default class ElectronEnergyLevelDiagramNode extends Node {
     this.electronNode = electronNode;
     this.stateLayer = stateLayer;
     this.squiggleLayer = squiggleLayer;
+    this.energies = calculateEnergies( MOTHAConstants.MAX_STATE );
   }
+
+  /**
+   * Gets the electron's energy in state n, in eV.
+   */
+  protected getEnergy( n: number ): number {
+    assert && assert( n >= MOTHAConstants.GROUND_STATE && n <= MOTHAConstants.MAX_STATE );
+    return this.energies[ n ];
+  }
+}
+
+/*
+ * Calculates the energy values the possible electron state n = 1 to n = maxState.
+ * Optional distortion is applied to increase the space between energy values so that they don't visually overlap.
+ */
+function calculateEnergies( maxState: number ): number[] {
+  const energies: number[] = [];
+  for ( let n = 1; n <= maxState; n++ ) {
+    energies[ n ] = E1 / ( n * n );
+    if ( DISTORTION_FACTOR > 0 ) {
+      energies[ n ] *= ( 1 + DISTORTION_FACTOR );
+    }
+  }
+  return energies;
 }
 
 modelsOfTheHydrogenAtom.register( 'ElectronEnergyLevelDiagramNode', ElectronEnergyLevelDiagramNode );
