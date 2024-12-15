a30d4d50227d39ddbb98b31b1aa5d27d6bfc9156
pixelzoom <cmalley@pixelzoom.com>
2024-12-15 15:15:43 -0700

    add level lines and n labels for Schrodinger

--- js/energylevels/view/SchrodingerElectronEnergyLevelDiagramNode.ts
+++ js/energylevels/view/SchrodingerElectronEnergyLevelDiagramNode.ts
@@ -10,6 +10,17 @@ import modelsOfTheHydrogenAtom from '../../modelsOfTheHydrogenAtom.js';
 import ElectronEnergyLevelDiagramNode, { ElectronEnergyLevelDiagramNodeOptions } from './ElectronEnergyLevelDiagramNode.js';
 import { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import SchrodingerModel from '../../common/model/SchrodingerModel.js';
+import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import PatternStringProperty from '../../../../axon/js/PatternStringProperty.js';
+import ModelsOfTheHydrogenAtomStrings from '../../ModelsOfTheHydrogenAtomStrings.js';
+import MOTHASymbols from '../../common/MOTHASymbols.js';
+import { HBox, Line, Node, RichText } from '../../../../scenery/js/imports.js';
+import MOTHAConstants from '../../common/MOTHAConstants.js';
+
+const LEVEL_NODE_X_OFFSET = 15;
+const LEVEL_LINE_LENGTH = 15;
+const LEVEL_LINE_X_SPACING = 4;
+const LEVEL_LABEL_FONT = new PhetFont( 12 );
 
 type SelfOptions = EmptySelfOptions;
 
@@ -19,10 +30,51 @@ export default class SchrodingerElectronEnergyLevelDiagramNode extends ElectronE
 
   public constructor( hydrogenAtom: SchrodingerModel, providedOptions: SchrodingerElectronEnergyLevelDiagramNodeOptions ) {
 
-    //TODO Implement diagram for Schrodinger.
-
     super( providedOptions );
+
+    for ( let n = MOTHAConstants.GROUND_STATE; n <= MOTHAConstants.MAX_STATE; n++ ) {
+      const levelNode = createLevelNode( n );
+      levelNode.localBoundsProperty.link( () => {
+        levelNode.left = this.energyAxisHBox.right + LEVEL_NODE_X_OFFSET;
+        levelNode.centerY = this.getYOffsetForState( n );
+      } );
+      this.stateLayer.addChild( levelNode );
+    }
+
+    //TODO Position electron on level line.
+
+    //TODO Display squiggle between previous and current electron state.
   }
+}
+
+/**
+ * Creates a Node for the level that corresponds to an electron state (n).
+ * This Node consists n-1 horizontal lines with label "n = {value}" to the right of the lines.
+ */
+function createLevelNode( n: number ): Node {
+
+  const linesLength = n * LEVEL_LINE_LENGTH + ( n - 1 ) * LEVEL_LINE_X_SPACING;
+  const lines = new Line( 0, 0, linesLength, 0, {
+    lineWidth: 1,
+    lineDash: [ LEVEL_LINE_LENGTH, LEVEL_LINE_X_SPACING ],
+    stroke: 'black'
+  } );
+
+  const labelStringProperty = new PatternStringProperty( ModelsOfTheHydrogenAtomStrings.nEqualsStringProperty, {
+    nSymbol: MOTHASymbols.nStringProperty,
+    nValue: n
+  } );
+  const label = new RichText( labelStringProperty, {
+    fill: 'black',
+    font: LEVEL_LABEL_FONT,
+    maxWidth: 50
+  } );
+
+  return new HBox( {
+    children: [ lines, label ],
+    align: 'center',
+    spacing: 5
+  } );
 }
 
 modelsOfTheHydrogenAtom.register( 'SchrodingerElectronEnergyLevelDiagramNode', SchrodingerElectronEnergyLevelDiagramNode );
