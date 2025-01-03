a0fe52d9ca321762b0bdbc28de099f3d2478ed0e
pixelzoom <cmalley@pixelzoom.com>
2025-01-02 17:43:50 -0700

    improve remapping of UV and IR wavelengths, https://github.com/phetsims/models-of-the-hydrogen-atom/issues/48

--- js/common/view/EmissionChart.ts
+++ js/common/view/EmissionChart.ts
@@ -44,8 +44,9 @@ type SelfOptions = {
   // Node that serves as the x-axis.
   xAxis: Node;
 
-  // Plots a wavelength value at some other value on the x-axis. Used to compress the x-axis for the UV and IR spectrums.
-  plotAtX?: ( wavelength: number ) => number;
+  // Maps a wavelength to some other value. Used to compress the x-axis for the UV and IR spectrums.
+  // See https://github.com/phetsims/models-of-the-hydrogen-atom/issues/48#issuecomment-2568401842.
+  wavelengthMap?: Map<number, number> | null;
 };
 
 type EmissionChartOptions = SelfOptions;
@@ -59,11 +60,24 @@ class EmissionChart extends Node {
 
       // SelfOptions
       hasTickMarks: true,
-      plotAtX: wavelength => {
-        assert && assert( providedOptions.wavelengths.includes( wavelength ), `${wavelength} is not a wavelength for this chart.` );
-        return wavelength;
-      }
+      wavelengthMap: null
     }, providedOptions );
+
+    // If wavelengthMap was not provided, create a default map that does not change wavelengths.
+    const wavelengthMap = options.wavelengthMap || new Map<number, number>( options.wavelengths.map( wavelength => [ wavelength, wavelength ] ) );
+
+    // Verify that all wavelengths are present in the map, and that they are mapped to <= value.
+    assert && assert( wavelengthMap.size === options.wavelengths.length );
+    assert && assert( _.every( [ ...wavelengthMap.keys() ], key => options.wavelengths.includes( key ) ) );
+    assert && assert( _.every( [ ...wavelengthMap.keys() ], key => wavelengthMap.get( key )! <= key ) );
+
+    // Handles the remapping of a wavelength.
+    const remap = ( wavelength: number ) => {
+      assert && assert( options.wavelengths.includes( wavelength ), `${wavelength} is not a wavelength for this chart.` );
+      const newWavelength = wavelengthMap.get( wavelength )!;
+      assert && assert( newWavelength !== undefined );
+      return newWavelength;
+    };
 
     const xAxis = options.xAxis;
     const xAxisLength = options.xAxis.width;
@@ -73,10 +87,10 @@ class EmissionChart extends Node {
 
     options.wavelengths.forEach( wavelength => {
 
-      assert && assert( options.plotAtX( wavelength ) >= options.minWavelength && options.plotAtX( wavelength ) <= options.maxWavelength,
+      assert && assert( remap( wavelength ) >= options.minWavelength && remap( wavelength ) <= options.maxWavelength,
         `tickValue is out of range: ${wavelength}` );
 
-      const x = Utils.linear( options.minWavelength, options.maxWavelength, 0, xAxisLength, options.plotAtX( wavelength ) );
+      const x = Utils.linear( options.minWavelength, options.maxWavelength, 0, xAxisLength, remap( wavelength ) );
 
       if ( options.hasTickMarks ) {
         const line = new Line( 0, 0, 0, TICK_LINE_LENGTH, {
@@ -107,7 +121,7 @@ class EmissionChart extends Node {
 
       const barNode = new SpectrometerBarNode( wavelength );
       barNode.localBoundsProperty.link( () => {
-        barNode.centerX = Utils.linear( options.minWavelength, options.maxWavelength, 0, xAxisLength, options.plotAtX( wavelength ) );
+        barNode.centerX = Utils.linear( options.minWavelength, options.maxWavelength, 0, xAxisLength, remap( wavelength ) );
         barNode.bottom = xAxis.top - 1;
       } );
       children.push( barNode );
@@ -141,23 +155,19 @@ class UVEmissionChart extends EmissionChart {
 
     const wavelengths = [ ...photonAbsorptionModel.getUVWavelengths(), PlumPuddingModel.PHOTON_EMISSION_WAVELENGTH ];
 
-    // Wavelengths are brute-force remapped to compress the UV spectrum.
+    // Wavelengths are remapped to compress the IR spectrum. This is a brute-force, hardcoded approach because we
+    // need fine control of the position of each tick mark on the axis. All possible wavelength are included here,
+    // even when their values are not changed, because that made it easier to adjust this map iteratively, and easier
+    // to validate for possible programming errors.
     // See https://github.com/phetsims/models-of-the-hydrogen-atom/issues/48#issuecomment-2568401842.
-    const plotAtX = ( wavelength: number ) => {
-      assert && assert( wavelengths.includes( wavelength ), `${wavelength} is not a wavelength for this chart.` );
-      if ( wavelength === 103 ) {
-        return 100;
-      }
-      else if ( wavelength === 122 ) {
-        return 105;
-      }
-      else if ( wavelength === 150 ) {
-        return 112;
-      }
-      else {
-        return wavelength;
-      }
-    };
+    const wavelengthMap = new Map<number, number>( [
+      [ 94, 94 ],
+      [ 95, 95 ],
+      [ 97, 97 ],
+      [ 103, 100 ],
+      [ 122, 105 ],
+      [ 150, 112 ]
+    ] );
 
     const xAxis = new Rectangle( 0, 0, axisLength, AXIS_HEIGHT, {
       fill: MOTHAColors.UV_COLOR
@@ -165,10 +175,10 @@ class UVEmissionChart extends EmissionChart {
 
     super( dataPointsProperty, combineOptions<EmissionChartOptions>( {
       wavelengths: wavelengths,
-      minWavelength: _.min( wavelengths )! - 1,
-      maxWavelength: plotAtX( _.max( wavelengths )! ) + 1,
+      minWavelength: wavelengthMap.get( _.min( wavelengths )! )! - 1,
+      maxWavelength: wavelengthMap.get( _.max( wavelengths )! )! + 1,
       xAxis: xAxis,
-      plotAtX: plotAtX
+      wavelengthMap: wavelengthMap
     }, providedOptions ) );
   }
 }
@@ -184,26 +194,15 @@ class IREmissionChart extends EmissionChart {
 
     const wavelengths = photonAbsorptionModel.getIRWavelengths();
 
-    // Wavelengths are brute-force remapped to compress the IR spectrum.
-    // See https://github.com/phetsims/models-of-the-hydrogen-atom/issues/48#issuecomment-2568401842.
-    const plotAtX = ( wavelength: number ) => {
-      assert && assert( wavelengths.includes( wavelength ), `${wavelength} is not a wavelength for this chart.` );
-      if ( wavelength === 1876 ) {
-        return 1650;
-      }
-      else if ( wavelength === 2626 ) {
-        return 2200;
-      }
-      else if ( wavelength === 4052 ) {
-        return 3000;
-      }
-      else if ( wavelength === 7460 ) {
-        return 4000;
-      }
-      else {
-        return wavelength;
-      }
-    };
+    // The comment about wavelengthMap in UVEmissionChart applies here.
+    const wavelengthMap = new Map<number, number>( [
+      [ 1094, 1094 ],
+      [ 1282, 1282 ],
+      [ 1876, 1650 ],
+      [ 2626, 2200 ],
+      [ 4052, 3000 ],
+      [ 7460, 4000 ]
+    ] );
 
     const xAxis = new Rectangle( 0, 0, axisLength, AXIS_HEIGHT, {
       fill: MOTHAColors.IR_COLOR
@@ -212,9 +211,9 @@ class IREmissionChart extends EmissionChart {
     super( dataPointsProperty, combineOptions<EmissionChartOptions>( {
       wavelengths: wavelengths,
       minWavelength: 1000,
-      maxWavelength: plotAtX( 7460 ) + 50,
+      maxWavelength: wavelengthMap.get( 7460 )! + 50,
       xAxis: xAxis,
-      plotAtX: plotAtX
+      wavelengthMap: wavelengthMap
     }, providedOptions ) );
   }
 }
@@ -230,7 +229,7 @@ class VisibleEmissionChart extends EmissionChart {
 
     const wavelengths = photonAbsorptionModel.getVisibleWavelengths();
     const minWavelength = VisibleColor.MIN_WAVELENGTH;
-    const maxWavelength = Math.min( VisibleColor.MAX_WAVELENGTH, _.max( wavelengths )! + 30 );
+    const maxWavelength = _.max( wavelengths )! + 30;
 
     const xAxis = new SpectrumNode( {
       size: new Dimension2( axisLength, AXIS_HEIGHT ),
