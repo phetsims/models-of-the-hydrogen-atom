2a6c9ed6c6ea0e9c8b2bdd08014dba4f31ce02a3
phet-dev <phetadmin@gmail.com>
2025-01-18 03:44:14 -0700

    update copyright dates from daily grunt work

--- js/common/view/PhotonNode.ts
+++ js/common/view/PhotonNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2022-2024, University of Colorado Boulder
+// Copyright 2022-2025, University of Colorado Boulder
 
 /**
  * PhotonNode is the visual representation of a photon. A round "orb" has an outer "halo" and a whimsical "sparkle"
