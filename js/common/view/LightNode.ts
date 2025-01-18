e45b6199d57e1228814404bdefd58ce21040b148
pixelzoom <cmalley@pixelzoom.com>
2025-01-18 13:01:28 -0700

    rename lampNode tandem

--- js/common/view/LightNode.ts
+++ js/common/view/LightNode.ts
@@ -19,7 +19,7 @@ export class LightNode extends Node {
 
   public constructor( light: Light, tandem: Tandem ) {
 
-    const laserPointerNode = new LaserPointerNode( light.isOnProperty, {
+    const lampNode = new LaserPointerNode( light.isOnProperty, {
       bodySize: new Dimension2( 88, 64 ),
       nozzleSize: new Dimension2( 18, 50 ),
       buttonOptions: {
@@ -28,19 +28,20 @@ export class LightNode extends Node {
       rotation: -Light.DIRECTION, // +y is up in the model, down in the view
 
       accessibleName: ModelsOfTheHydrogenAtomStrings.a11y.lightSource.accessibleNameStringProperty,
-      tandem: tandem.createTandem( 'laserPointerNode' ),
+      tandem: tandem.createTandem( 'lampNode' ),
+      tandemNameSuffix: 'Node',
       phetioVisiblePropertyInstrumented: false
     } );
 
     // Beam of light
     const beamNode = new BeamNode( light, {
-      centerX: laserPointerNode.centerX,
-      bottom: laserPointerNode.top + 1
+      centerX: lampNode.centerX,
+      bottom: lampNode.top + 1
     } );
 
     super( {
       isDisposable: false,
-      children: [ beamNode, laserPointerNode ],
+      children: [ beamNode, lampNode ],
       tandem: tandem,
       phetioFeatured: true,
       phetioVisiblePropertyInstrumented: false
