8185bdf9b7b53df012f4366cf421bf21d9f6a2e7
pixelzoom <cmalley@pixelzoom.com>
2025-01-21 11:53:04 -0700

    phetioDocumentation

--- js/common/view/ContinuumBarNode.ts
+++ js/common/view/ContinuumBarNode.ts
@@ -56,6 +56,7 @@ export default class ContinuumBarNode extends Node {
       isDisposable: false,
       children: [ barNode, classicalText, quantumText ],
       tandem: tandem,
+      phetioDocumentation: 'The vertical bar that indicates the continuum from Classical to Quantum atomic model.',
       visiblePropertyOptions: {
         phetioFeatured: true
       }
