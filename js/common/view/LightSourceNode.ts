702474a0b378d9859395146385af2ad6a15ae031
pixelzoom <cmalley@pixelzoom.com>
2025-01-21 12:04:06 -0700

    move BeamNode into LightSourceNode, factor out LampNode

--- js/common/view/LightSourceNode.ts
+++ js/common/view/LightSourceNode.ts
@@ -8,32 +8,25 @@
 
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import LaserPointerNode from '../../../../scenery-phet/js/LaserPointerNode.js';
-import { Node } from '../../../../scenery/js/imports.js';
+import { Node, NodeTranslationOptions, Rectangle, RectangleOptions } from '../../../../scenery/js/imports.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import modelsOfTheHydrogenAtom from '../../modelsOfTheHydrogenAtom.js';
 import ModelsOfTheHydrogenAtomStrings from '../../ModelsOfTheHydrogenAtomStrings.js';
 import LightSource from '../model/LightSource.js';
-import BeamNode from './BeamNode.js';
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import { combineOptions } from '../../../../phet-core/js/optionize.js';
+import Property from '../../../../axon/js/Property.js';
+
+const BEAM_SIZE = new Dimension2( 30, 65 );
 
 export class LightSourceNode extends Node {
 
   public constructor( lightSource: LightSource, tandem: Tandem ) {
 
-    // Lamp that emits the light
-    const lampNode = new LaserPointerNode( lightSource.isOnProperty, {
-      bodySize: new Dimension2( 88, 64 ),
-      nozzleSize: new Dimension2( 18, 50 ),
-      buttonOptions: {
-        radius: 19
-      },
-      rotation: -LightSource.DIRECTION, // +y is up in the model, down in the view
-      accessibleName: ModelsOfTheHydrogenAtomStrings.a11y.lightSource.accessibleNameStringProperty,
-      tandem: tandem.createTandem( 'lampNode' ),
-      tandemNameSuffix: 'Node',
-      phetioVisiblePropertyInstrumented: false
-    } );
+    // Lamp that emits the light.
+    const lampNode = new LampNode( lightSource.isOnProperty, tandem.createTandem( 'lampNode' ) );
 
-    // Beam of light
+    // Beam of light that comes out of the lamp.
     const beamNode = new BeamNode( lightSource, {
       centerX: lampNode.centerX,
       bottom: lampNode.top + 1
@@ -50,5 +43,39 @@ export class LightSourceNode extends Node {
     this.addLinkedElement( lightSource );
   }
 }
+
+class LampNode extends LaserPointerNode {
+
+  public constructor( isOnProperty: Property<boolean>, tandem: Tandem ) {
+    super( isOnProperty, {
+      bodySize: new Dimension2( 88, 64 ),
+      nozzleSize: new Dimension2( 18, 50 ),
+      buttonOptions: {
+        radius: 19
+      },
+      rotation: -LightSource.DIRECTION, // +y is up in the model, down in the view
+      accessibleName: ModelsOfTheHydrogenAtomStrings.a11y.lightSource.accessibleNameStringProperty,
+      tandem: tandem.createTandem( 'lampNode' ),
+      tandemNameSuffix: 'Node',
+      phetioVisiblePropertyInstrumented: false
+    } );
+  }
+}
+
+class BeamNode extends Rectangle {
+
+  public constructor( lightSource: LightSource, providedOptions?: NodeTranslationOptions ) {
+    super( 0, 0, BEAM_SIZE.width, BEAM_SIZE.height, combineOptions<RectangleOptions>( {
+      isDisposable: false,
+      visibleProperty: lightSource.isOnProperty,
+      fill: lightSource.colorProperty,
+
+      // When displaying a white beam on a white background, a stroke is needed.
+      stroke: new DerivedProperty( [ lightSource.lightModeProperty ], lightMode => lightMode === 'white' ? 'black' : null ),
+      lineWidth: 0.5
+    }, providedOptions ) );
+  }
+}
+
 
 modelsOfTheHydrogenAtom.register( 'LightSourceNode', LightSourceNode );
