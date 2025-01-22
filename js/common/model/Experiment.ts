b2424f73997f27a3cd8f1151eba83703b642a1fa
Chris Malley <cmalley@pixelzoom.com>
2025-01-22 15:25:36 -0700

    TODO

--- js/common/model/Experiment.ts
+++ js/common/model/Experiment.ts
@@ -1,5 +1,6 @@
 // Copyright 2022-2025, University of Colorado Boulder
 
+//TODO Since this inherits from Schrodinger, it gets a MetastableHandler, and will automatically excite the atom when the light source is white light. Is that OK? Is that what Java did?
 /**
  * Experiment is the model when we are in "Experiment" mode. It is identical to the Schrodinger model.
  *
