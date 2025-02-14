494205c3ce4c19f7268cdb54570eb1d280e527e3
pixelzoom <cmalley@pixelzoom.com>
2025-02-14 13:40:01 -0700

    reimplement orbitals using dataURL, https://github.com/phetsims/models-of-the-hydrogen-atom/issues/129

--- js/common/view/SchrodingerOpacityCache.ts
+++ js/common/view/SchrodingerOpacityCache.ts
@@ -1,17 +1,7 @@
-// Copyright 2022-2025, University of Colorado Boulder
+// Copyright 2025, University of Colorado Boulder
 
 /**
- * SchrodingerOpacityCache is responsible for representing each Schrodinger (n,l,m) state as a 2D grid of opacity values
- * in the range [0,1], where 0 is transparent and 1 is opaque. The grid is used to render the electron's orbital.
- *
- * A grid is computed on demand for each (n,l,m) state, then cached. Alternatively, calling the populate method
- * will eagerly compute values for all (n,l,m) states reachable in the sim, improving runtime performance at
- * the expense of startup time.
- *
- * A singleton instance is used throughout the sim, so that we only compute the grid once for each (n,l,m) state,
- * and both screens can benefit from the same cache.
- *
- * In the Java implementation, this was class BrightnessCache in SchrodingerNode.java.
+ * TODO https://github.com/phetsims/models-of-the-hydrogen-atom/issues/129
  *
  * Useful references for orbital shapes:
  * https://upload.wikimedia.org/wikipedia/commons/e/e7/Hydrogen_Density_Plots.png
@@ -35,20 +25,19 @@ const NUMBER_OF_CELLS = MOTHAQueryParameters.gridSize;
 
 const QUADRANT_SIDE_LENGTH = ZoomedInBox.SIDE_LENGTH / 2; // in model coordinates!
 
+// The length of one side of a cell, which is a cube.
+const CELL_SIDE_LENGTH = QUADRANT_SIDE_LENGTH / NUMBER_OF_CELLS;
+
 // A 2D grid of opacity values that describes the orbital for a specific (n,l,m) state, in [row][column] order.
 export type OpacityGrid = Array<Array<number>>;
 
 class SchrodingerOpacityCache {
 
-  // Cache of 2D opacity grids, indexed by [n-1][l][abs(m)]. This data structure is large, but heap snapshots in
-  // Chrome Dev Tools show that the sim still has a reasonable memory footprint.
-  private readonly cache: Array<Array<Array<OpacityGrid | null>>>;
+  // Cache of dataURLs, indexed by [n-1][l][abs(m)]. These dataURLs point to PNG files for the orbitals.
+  private readonly cache: Array<Array<Array<string | null>>>;
 
   // Reusable array for summing probability densities.
   private readonly sums: Array<Array<number>>;
-
-  // The length of one side of a cell, which is a cube.
-  private readonly cellSideLength: number;
 
   public constructor() {
 
@@ -71,9 +60,6 @@ class SchrodingerOpacityCache {
       this.sums[ i ] = new Array( NUMBER_OF_CELLS ).fill( 0 );
     }
 
-    // 3D cell size, where each cell is a cube.
-    this.cellSideLength = QUADRANT_SIDE_LENGTH / NUMBER_OF_CELLS;
-
     // Eagerly populate the cache.
     if ( MOTHAQueryParameters.computeOrbitals === 'atStartup' ) {
       this.populate();
@@ -87,39 +73,56 @@ class SchrodingerOpacityCache {
     for ( let n = 1; n <= QuantumElectron.MAX_STATE; n++ ) {
       for ( let l = 0; l <= Math.min( n - 1, SchrodingerQuantumNumbers.lMax ); l++ ) {
         for ( let m = 0; m <= l; m++ ) {
-          this.getOpacityGrid( new SchrodingerQuantumNumbers( n, l, m ) );
+          this.getDataURL( new SchrodingerQuantumNumbers( n, l, m ) );
         }
       }
     }
   }
 
   /**
-   * Gets the 2D opacity grid for a state. If it's not already cached, compute it and cache it.
+   * Gets a dataURL containing an orbital's image in PNG format. If not previously cached, it will be created and cached.
+   * This PNG file shows the complete orbital, for all 4 quadrants.
    */
-  public getOpacityGrid( nlm: SchrodingerQuantumNumbers ): OpacityGrid {
-    let opacityGrid = this.getCachedOpacityGrid( nlm );
-    assert && assert( opacityGrid !== undefined );
-    if ( opacityGrid === null ) {
-      opacityGrid = this.computeOpacityGrid( nlm );
-      this.setCachedOpacityGrid( nlm, opacityGrid );
+  public getDataURL( nlm: SchrodingerQuantumNumbers ): string {
+
+    let dataURL = this.getCachedDataURL( nlm );
+    if ( !dataURL ) {
+      const r = MOTHAColors.electronBaseColorProperty.value.r;
+      const g = MOTHAColors.electronBaseColorProperty.value.g;
+      const b = MOTHAColors.electronBaseColorProperty.value.b;
+
+      const opacityGrid = this.computeOpacityGrid( nlm );
+      const opacityArray = opacityGrid.flat(); // 2D to 1D
+      const rgbaArray = opacityArray.map( opacity => [ r, g, b, opacity * 255 ] ).flat(); // 1D array of color components in r,g,b,a order
+      const imageData = new ImageData( 2 * NUMBER_OF_CELLS, 2 * NUMBER_OF_CELLS );
+      imageData.data.set( rgbaArray );
+
+      const canvas = document.createElement( 'canvas' );
+      canvas.width = 2 * NUMBER_OF_CELLS;
+      canvas.height = 2 * NUMBER_OF_CELLS;
+      const context = canvas.getContext( '2d', { alpha: true } )!;
+      context.putImageData( imageData, 0, 0 );
+      dataURL = canvas.toDataURL( 'image/png' );
+
+      this.setCachedDataURL( nlm, dataURL );
     }
-    return opacityGrid;
+    return dataURL;
   }
 
   /**
-   * Sets opacity values for an electron state (n,l,m) in the cache.
+   * Sets the data URL (PNG image) for an electron state (n,l,m) in the cache.
    * Note that the cache is indexed by n-1, because the range of n is [1,6].
    */
-  private setCachedOpacityGrid( nlm: SchrodingerQuantumNumbers, opacityGrid: OpacityGrid ): void {
+  private setCachedDataURL( nlm: SchrodingerQuantumNumbers, dataURL: string ): void {
     phet.log && phet.log( `Populating orbital cache for (n,l,m) = ${nlm.toString()}` );
-    this.cache[ nlm.n - 1 ][ nlm.l ][ Math.abs( nlm.m ) ] = opacityGrid;
+    this.cache[ nlm.n - 1 ][ nlm.l ][ Math.abs( nlm.m ) ] = dataURL;
   }
 
   /**
-   * Gets opacity values for an electron state (n,l,m) from the cache.
+   * Gets the data URL (PNG image) for an electron state (n,l,m) in the cache.
    * Note that the cache is indexed by n-1, because the range of n is [1,6].
    */
-  private getCachedOpacityGrid( nlm: SchrodingerQuantumNumbers ): OpacityGrid | null {
+  private getCachedDataURL( nlm: SchrodingerQuantumNumbers ): string | null {
     return this.cache[ nlm.n - 1 ][ nlm.l ][ Math.abs( nlm.m ) ];
   }
 
@@ -131,15 +134,15 @@ class SchrodingerOpacityCache {
     // The maximum sum, to be used for normalizing.
     let maxSum = 0;
 
-    // Calculate the probability density at each point in 3D space. Then sum the values along the depth (y) axis
-    // that have the same (x,z) coordinates.
+    // Calculate the probability density at each point in 3D space. Then project to 2D by summing the values along
+    // the depth (y) axis that have the same (x,z) coordinates. This is for the rightBottom quadrant of the 2D grid.
     for ( let row = 0; row < NUMBER_OF_CELLS; row++ ) {
-      const z = ( row * this.cellSideLength ) + ( this.cellSideLength / 2 );
+      const z = ( row * CELL_SIDE_LENGTH ) + ( CELL_SIDE_LENGTH / 2 );
       for ( let column = 0; column < NUMBER_OF_CELLS; column++ ) {
-        const x = ( column * this.cellSideLength ) + ( this.cellSideLength / 2 );
+        const x = ( column * CELL_SIDE_LENGTH ) + ( CELL_SIDE_LENGTH / 2 );
         let sum = 0;
         for ( let depth = 0; depth < NUMBER_OF_CELLS; depth++ ) {
-          const y = ( depth * this.cellSideLength ) + ( this.cellSideLength / 2 );
+          const y = ( depth * CELL_SIDE_LENGTH ) + ( CELL_SIDE_LENGTH / 2 );
           const probabilityDensity = SchrodingerModel.solveProbabilityDensity( nlm, x, y, z );
           sum += probabilityDensity;
         }
@@ -150,10 +153,10 @@ class SchrodingerOpacityCache {
       }
     }
 
-    // Create the 2D grid, initially filled with zeros
-    const opacityGrid = new Array( NUMBER_OF_CELLS );
+    // Create the 2D grid for the rightBottom quadrant, initially filled with zeros
+    const rightBottomGrid = new Array<Array<number>>( NUMBER_OF_CELLS );
     for ( let i = 0; i < NUMBER_OF_CELLS; i++ ) {
-      opacityGrid[ i ] = new Array( NUMBER_OF_CELLS ).fill( 0 );
+      rightBottomGrid[ i ] = new Array<number>( NUMBER_OF_CELLS ).fill( 0 );
     }
 
     // Populate the 2D grid with normalized opacity values in the range [0,1].
@@ -163,39 +166,27 @@ class SchrodingerOpacityCache {
         if ( maxSum > 0 ) {
           opacity = this.sums[ row ][ column ] / maxSum;
         }
-        opacityGrid[ row ][ column ] = opacity;
+        rightBottomGrid[ row ][ column ] = opacity;
       }
     }
 
-    return opacityGrid;
-  }
+    // 2D data for the leftBottom quadrant.
+    const leftBottomGrid = rightBottomGrid.map( rightHalfRow => Array.from( rightHalfRow ).reverse() );
 
-  /**
-   * Gets a data URL containing an orbital's image in PNG format.
-   * Returns null if the orbital grid has not yet been cached.
-   */
-  public getDataURL( nlm: SchrodingerQuantumNumbers ): string | null {
-    const opacityGrid = this.getCachedOpacityGrid( nlm );
-    if ( !opacityGrid ) {
-      return null;
+    // Data for bottom half of the 2D projection.
+    const bottomHalfGrid: OpacityGrid = [];
+    for ( let i = 0; i < leftBottomGrid.length; i++ ) {
+      const leftRow = leftBottomGrid[ i ];
+      const rightRow = rightBottomGrid[ i ];
+      const row = leftRow.concat( rightRow );
+      bottomHalfGrid.push( row );
     }
-    else {
-      const r = MOTHAColors.electronBaseColorProperty.value.r;
-      const g = MOTHAColors.electronBaseColorProperty.value.g;
-      const b = MOTHAColors.electronBaseColorProperty.value.b;
 
-      const opacityArray = opacityGrid.flat(); // 2D to 1D
-      const rgbaArray = opacityArray.map( opacity => [ r, g, b, opacity * 255 ] ).flat(); // 1D array of color components in r,g,b,a order
-      const imageData = new ImageData( NUMBER_OF_CELLS, NUMBER_OF_CELLS );
-      imageData.data.set( rgbaArray );
+    // Data for the top half of the 2D projection.
+    const topHalfGrid: OpacityGrid = Array.from( bottomHalfGrid ).reverse();
 
-      const canvas = document.createElement( 'canvas' );
-      canvas.width = NUMBER_OF_CELLS;
-      canvas.height = NUMBER_OF_CELLS;
-      const context = canvas.getContext( '2d', { alpha: true } )!;
-      context.putImageData( imageData, 0, 0 );
-      return canvas.toDataURL( 'image/png' );
-    }
+    // Data for the full grid, which completely describes the orbital.
+    return [ ...topHalfGrid, ...bottomHalfGrid ];
   }
 }
 
@@ -203,4 +194,4 @@ class SchrodingerOpacityCache {
 const schrodingerOpacityCache = new SchrodingerOpacityCache();
 
 modelsOfTheHydrogenAtom.register( 'SchrodingerOpacityCache', SchrodingerOpacityCache );
-export default schrodingerOpacityCache;
+export { schrodingerOpacityCache };
--- js/common/view/SchrodingerOrbitalNode.ts
+++ js/common/view/SchrodingerOrbitalNode.ts
@@ -3,111 +3,45 @@
 /**
  * SchrodingerOrbitalNode renders the atomic orbital, a function describing the location and wave-like behavior of the
  * electron. The orbital is characterized by the quantum numbers (n,l,m) that describe the electron's state - see
- * SchrodingerQuantumNumbers. In this implementation, the orbital is rendered as a 2D NxN grid, where each cell in
- * the grid is filled with the electron color with varying alpha (opacity).
- *
- * In the Java implementation, this was inner classes AtomNode and GridNode in SchrodingerNode.java.
+ * SchrodingerQuantumNumbers.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
-import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import ModelViewTransform2 from '../../../../phetcommon/js/view/ModelViewTransform2.js';
-import CanvasNode from '../../../../scenery/js/nodes/CanvasNode.js';
 import modelsOfTheHydrogenAtom from '../../modelsOfTheHydrogenAtom.js';
 import SchrodingerQuantumNumbers from '../model/SchrodingerQuantumNumbers.js';
-import MOTHAColors from '../MOTHAColors.js';
 import ZoomedInBoxNode from './ZoomedInBoxNode.js';
-import schrodingerOpacityCache, { OpacityGrid } from './SchrodingerOpacityCache.js';
+import MOTHAQueryParameters from '../MOTHAQueryParameters.js';
+import Image from '../../../../scenery/js/nodes/Image.js';
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import { schrodingerOpacityCache } from './SchrodingerOpacityCache.js';
 
+const NUMBER_OF_CELLS = MOTHAQueryParameters.gridSize;
 const QUADRANT_SIDE_LENGTH = ZoomedInBoxNode.SIDE_LENGTH / 2; // in view coordinates!
 
-export default class SchrodingerOrbitalNode extends CanvasNode {
-
-  private opacityGrid: OpacityGrid; // 2D grid of opacity values, in [row][column] order.
-  private cellWidth: number;
-  private cellHeight: number;
+export default class SchrodingerOrbitalNode extends Image {
 
   public constructor( nlmProperty: TReadOnlyProperty<SchrodingerQuantumNumbers>,
                       atomPosition: Vector2,
                       modelViewTransform: ModelViewTransform2 ) {
 
-    super( {
-      pickable: false,
-      canvasBounds: new Bounds2( -QUADRANT_SIDE_LENGTH, -QUADRANT_SIDE_LENGTH, QUADRANT_SIDE_LENGTH, QUADRANT_SIDE_LENGTH ),
-      translation: modelViewTransform.modelToViewPosition( atomPosition )
+    const imageProperty = new DerivedProperty( [ nlmProperty ], nlm => schrodingerOpacityCache.getDataURL( nlm ) );
+
+    super( imageProperty, {
+      isDisposable: false,
+      pickable: false
     } );
 
-    // These values will be populated by update().
-    this.opacityGrid = [];
-    this.cellWidth = 0;
-    this.cellHeight = 0;
+    this.setScaleMagnitude( QUADRANT_SIDE_LENGTH / NUMBER_OF_CELLS );
 
-    nlmProperty.link( nlm => this.update( nlm ) );
-
-    // If the electron color changes, trigger a call to paintCanvas.
-    MOTHAColors.electronBaseColorProperty.link( () => this.invalidatePaint() );
-  }
-
-  /**
-   * Updates the fields that are used to render the orbital, then triggers a call to paintCanvas.
-   */
-  private update( nlm: SchrodingerQuantumNumbers ): void {
-
-    // Get the 2D opacity grid that describe the orbital for the electron's state.
-    this.opacityGrid = schrodingerOpacityCache.getOpacityGrid( nlm );
-
-    // opacityGrid is for 1 quadrant, so use quadrant size to compute cell size.
-    this.cellWidth = QUADRANT_SIDE_LENGTH / this.opacityGrid[ 0 ].length;
-    this.cellHeight = QUADRANT_SIDE_LENGTH / this.opacityGrid.length;
-    assert && assert( this.cellWidth === this.cellHeight, 'cells must be square.' );
-
-    // This results in a call to paintCanvas.
-    this.invalidatePaint();
-  }
-
-  /**
-   * According to the documentation in superclass CanvasNode, paintCanvas should have no side effects other than
-   * drawing to the context.
-   */
-  public override paintCanvas( context: CanvasRenderingContext2D ): void {
-
-    // globalAlpha will be used to set the alpha component.
-    context.fillStyle = MOTHAColors.electronBaseColorProperty.value.toCSS();
-
-    // Values used for drawing each cell.
-    let x: number;
-    let z: number;
-    const w = this.cellWidth;
-    const h = this.cellHeight;
-
-    // For each cell in the 2D grid...
-    const numberOfRows = this.opacityGrid.length;
-    for ( let row = 0; row < numberOfRows; row++ ) {
-      const numberOfColumns = this.opacityGrid[ row ].length;
-      for ( let column = 0; column < numberOfColumns; column++ ) {
-
-        const opacity = this.opacityGrid[ row ][ column ];
-
-        // Skip cells that contain no information.
-        if ( opacity > 0 ) {
-
-          // Coordinates in the right-bottom quadrant.
-          x = ( column * this.cellWidth );
-          z = ( row * this.cellHeight );
-
-          // Fill a rectangle in each quadrant.
-          // Use globalAlpha and fillRect because we're filling rectangles with different alpha values.
-          context.globalAlpha = opacity;
-          context.fillRect( x, z, w, h ); // right-bottom quadrant
-          context.fillRect( -( x + this.cellWidth ), z, w, h ); // left-bottom quadrant
-          context.fillRect( x, -( z + this.cellHeight ), w, h ); // right-top quadrant
-          context.fillRect( -( x + this.cellWidth ), -( z + this.cellHeight ), w, h ); // left-top quadrant
-        }
-      }
-    }
+    // Center the image at the atom's position.
+    const atomViewPosition = modelViewTransform.modelToViewPosition( atomPosition );
+    this.localBoundsProperty.link( () => {
+      this.center = atomViewPosition;
+    } );
   }
 }
 
